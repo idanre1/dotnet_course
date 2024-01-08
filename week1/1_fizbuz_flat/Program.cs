2cf8c9572bbf55bb25e2e9fd7278f5aa4a54d47a
Idan Regev <idanre1@gmail.com>
2024-01-08 15:24:25 +0200

    week1

---
+++ week1/1_fizbuz_flat/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Read param n from cmd line
+int n = int.Parse(args[0]);
+
+// loop from 1 to n
+for (int i = 1; i <= n; i++)
+{
+    // if i is divisible by 3 and 7, print FizzBuzz
+    if (i % 3 == 0 && i % 7 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    // if i is divisible by 3, print Fizz
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    // if i is divisible by 7, print Buzz
+    else if (i % 7 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    // else print i
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
---
+++ week1/2_fizbuz_class/Program.cs
@@ -0,0 +1,45 @@
+﻿// read n from command line
+int n = int.Parse(args[0]);
+// init abc with values 2,3
+var abc = new Abc { Fizz = 2, Buzz = 3 };
+// run abc
+abc.Run(n);
+
+// print line seprator
+Console.WriteLine("----------");
+
+var abc2 = new Abc { Fizz = 3, Buzz = 5 };
+// run abc
+abc2.Run(2*n);
+
+
+class Abc
+{
+    public required int Fizz { get; init; }
+    public int Buzz { get; init; } = 7;
+
+    public void Run(int n)
+    {
+        // loop from 1 to n
+        for (int i = 1; i <= n; i++)
+        {
+            // Write i
+            Console.Write(i);
+            Console.Write(" ");
+
+            // if i is divisible by 3, print Fizz
+            if (i % Fizz == 0)
+            {
+                Console.Write("Fizz");
+            }
+            // if i is divisible by 7, print Buzz
+            if (i % Buzz == 0)
+            {
+                Console.Write("Buzz");
+            }
+
+            Console.WriteLine(" ");
+        }
+    }
+}
+
