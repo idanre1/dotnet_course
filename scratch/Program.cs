329f5f0c227d21dc0ab161704d270263e36b4614
Idan Regev <iregev@microsoft.com>
2024-01-30 09:57:44 +0200

    scratch pad

---
+++ scratch/Program.cs
@@ -0,0 +1,11 @@
+﻿// var bill = new Person("William Henry", "Gates"); //error
+
+public class Person
+{
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public string FullName => $"{FirstName} {LastName}";
+    public Person(string firstName, string lastName)
+    => (FirstName, LastName) = (firstName, lastName);
+}
+
